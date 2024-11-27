fd72f6e46578fceb97fc65952062fce7b1c31623
alvaro gallo <alvarogallo@hotmail.com>
2024-11-27 08:31:09 -0500

    Comprobando variables 2

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '[Password is set]' : 'Not
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
@@ -23,7 +24,7 @@ async function testConnection() {
   } catch (error) {
     console.error('❌ Database connection failed:', error.message);
   }
-  
+
 }
 
 testConnection();
