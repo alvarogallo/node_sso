ad60cd6ce8e18e6be6ff8e57a65ef384cdcc1f89
alvaro gallo <alvarogallo@hotmail.com>
2024-11-27 08:45:28 -0500

    Comprobando variables 4

--- src/server.js
+++ src/server.js
@@ -10,11 +10,19 @@ console.log('DB_NAME:', process.env.DB_NAME || 'Not set');
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '[Password is set]' : 'Not set');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+    host: process.env.DB_HOST.split(':')[0], // Separamos el host del puerto
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 async function testConnection() {
