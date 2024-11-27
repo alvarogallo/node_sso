a370cfcdd995751ad51fb3a3a6772867c255e87a
alvaro gallo <alvarogallo@hotmail.com>
2024-11-27 09:20:17 -0500

    Comprobando variables 9

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const [host] = process.env.MYSQLHOST.split(':');
 const pool = mysql.createPool({
   host,
   port: 36100,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
