20128896572850ccc3725398074960d5219ff130
alvaro gallo <alvarogallo@hotmail.com>
2024-11-27 08:51:34 -0500

    Comprobando variables 6

--- src/server.js
+++ src/server.js
@@ -9,22 +9,25 @@ console.log('DB_USER:', process.env.DB_USER || 'Not set');
 console.log('DB_NAME:', process.env.DB_NAME || 'Not set');
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '[Password is set]' : 'Not set');
 
-const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-    host: process.env.DB_HOST.split(':')[0], // Separamos el host del puerto
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectTimeout: 10000,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+// //   host: process.env.DB_HOST,
+// //   port: process.env.DB_PORT,
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME
+//     host: process.env.DB_HOST.split(':')[0], // Separamos el host del puerto
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectTimeout: 10000,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+const connectionString = process.env.MYSQL_URL || process.env.MYSQL_PUBLIC_URL;
+const pool = mysql.createPool(connectionString);
 
 async function testConnection() {
   try {
